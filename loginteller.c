7b40d6b5f33c241320b5bb9968026de6ab1ca89d
marcam23daw2 <15582055.clot@fje.edu>
2023-09-20 18:53:41 +0200

    Commit 2 del projecte loginteller

--- loginteller.c
+++ loginteller.c
@@ -1,7 +1,7 @@
 /* loginteller.c
  * Based on monousuar.c at http://www.binefa.net/gnu/gcc/processos/Informacio_d_usuari.html
  * www.binefa.cat
- * 20120507
+ * 20230920
 */
 #include <stdio.h>
 #include <unistd.h>    // getlogin()
@@ -14,7 +14,7 @@ int main(){
         perror("getlogin");
         exit(EXIT_FAILURE);
     }  
-    printf("Your login name at the system is : %s\n",szLogin);
+    printf("Your login name at the system is : %s\n",szUserName);
 
     return (0);
 }
